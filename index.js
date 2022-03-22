dbe6ff686a0c294548625e9e285a5ab4f15cc7da
inwerejosic <inwerejosic@gmail.com>
2022-03-22 14:23:09 +0100

    This commit captures the part of this project, where I built a route to read the package.json file of this project.

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const package = require('./package.json')
+
 
 const port = process.env.port || process.env.PORT || 5000
-
+const apiRoot = '/api'
 const app = express()
+
 
 // Configuring app
 app.use(bodyParser.urlencoded({ extended:true }))
 app.use(bodyParser.json())
 app.use(cors({ origin: /http:\/\/localhost/ }))
 app.options('*', cors())
+
+const router = express.Router()
+router.get('/', (req, res) =>{
+    res.send(`${package.au} - v${package.version}`)
+})
+
+// Getting the routes up and running
+app.use(apiRoot, router)
+
 
 // Starting the server
 app.listen(port, () =>{
